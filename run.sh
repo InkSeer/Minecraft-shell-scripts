7068b62134615ec030e5ed1dd70f5fc5f707e50a
InkSeer <132846796+InkSeer@users.noreply.github.com>
2023-09-17 14:31:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 screen -dmS mc_console java -Xmx1024M -Xms2024M -jar minecraft_server.jar nogui
+
+discord_webhook="your webhook link"
+
+curl -H "Content-Type: application/json" -d '{"username": "Server", "content": "Server is on"}' $discord_webhook
+
