c28288356b6bacd555a1bbe761dca7e7d1336578
InkSeer <132846796+InkSeer@users.noreply.github.com>
2023-09-17 14:32:43 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 screen -S mc_console -X stuff 'stop'`echo -ne'\015'`
+
+# Only if you want server status in discord (can be removed)
+discord_webhook="your webhook link"
+
+curl -H "Content-Type: application/json" -d '{"username": "Server", "content": "Server is off"}' $discord_webhook
