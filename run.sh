d1bbc67dae493efa88d44279caca3da5053c698f
InkSeer <132846796+InkSeer@users.noreply.github.com>
2023-09-17 14:47:12 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ screen -dmS mc_console java -Xmx1024M -Xms2024M -jar minecraft_server.jar nogui
 
 # Only if you want server status in discord (can be removed)
 discord_webhook="your webhook link"
-curl -H "Content-Type: application/json" -d '{"username": "Server", "content": "Server is on"}' $discord_webhook
+curl -H "Content-Type: application/json" -d '{"content": "Server is on"}' $discord_webhook
 
