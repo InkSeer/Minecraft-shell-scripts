d9e1bc920703e9d6abb9c64d508c9eaa0b601bb7
InkSeer <132846796+InkSeer@users.noreply.github.com>
2023-09-28 01:25:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-screen -dmS mc_console java -Xmx1024M -Xms2024M -jar minecraft_server.jar nogui
+screen -dmS mc_console java -Xmx2048M -Xms2048M -jar minecraft_server.jar nogui
 
 # Only if you want server status in discord (can be removed)
 discord_webhook="your webhook link"
