23f74d9557c6e842025fc750ae735c2258ff606a
InkSeer <132846796+InkSeer@users.noreply.github.com>
2023-09-28 23:06:03 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,14 @@ screen -dmS mc_console java -Xmx2048M -Xms2048M -jar minecraft_server.jar nogui
 
 # Only if you want server status in discord (can be removed)
 discord_webhook="your webhook link"
-curl -H "Content-Type: application/json" -d '{"embeds": [{"title": "Server is on","color": 5635925}]}' $discord_webhook
+discord_webhook_error="your webhook link"
 
+if screen -list | grep -q "mc"; then
+  # Server has started
+  curl -H "Content-Type: application/json" -d '{"embeds": [{"title": "Server is on","color": 5635925}]}' $discord_webhook
+  exit 0
+else
+  # Server failed to start
+  curl -H "Content-Type: application/json" -d '{"content": "Error 101: Server failed to start"}' $discord_webhook_error
+  exit 0
+fi
