0112f67e76ca369eee257b2f18e688381067d27e
InkSeer <132846796+InkSeer@users.noreply.github.com>
2023-09-28 23:08:43 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@ discord_webhook="your webhook link"
 discord_webhook_error="your webhook link"
 
 if screen -list | grep -q "mc"; then
-  # Server has started
-  curl -H "Content-Type: application/json" -d '{"embeds": [{"title": "Server is on","color": 5635925}]}' $discord_webhook
-  exit 0
+        # Server has started
+        curl -H "Content-Type: application/json" -d '{"embeds": [{"title": "Server is on","color": 5635925}]}' $discord_webhook
+        exit 0
 else
-  # Server failed to start
-  curl -H "Content-Type: application/json" -d '{"content": "Error 101: Server failed to start"}' $discord_webhook_error
-  exit 0
+        # Server failed to start
+        curl -H "Content-Type: application/json" -d '{"content": "Error 101: Server failed to start"}' $discord_webhook_error
+        exit 0
 fi
