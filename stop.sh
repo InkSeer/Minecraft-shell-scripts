c8d81f9ee503e195fe5c59beb5d4d3a95b170780
InkSeer <132846796+InkSeer@users.noreply.github.com>
2023-09-28 23:08:08 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -4,4 +4,14 @@ screen -S mc_console -X stuff 'stop'`echo -ne'\015'`
 
 # Only if you want server status in discord (can be removed)
 discord_webhook="your webhook link"
-curl -H "Content-Type: application/json" -d '{"embeds": [{"title": "Server is off","color": 16733525}]}' $discord_webhook
+discord_webhook_error="your webhook link"
+
+if ! screen -list | grep -q "mc"; then
+        # Server has stopped
+        curl -H "Content-Type: application/json" -d '{"embeds": [{"title": "Server is off","color": 16733525}]}' $discord_webhook
+        exit 0
+else
+        # Server failed to stop
+        curl -H "Content-Type: application/json" -d '{"content": "Error 102: Server failed to start"}' $discord_webhook_error
+        exit 0
+fi
