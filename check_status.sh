0b0bde5fe6dabebbb71e4d5f3657a7a774a2c184
InkSeer <132846796+InkSeer@users.noreply.github.com>
2023-09-29 14:13:56 +0200

    Create check_status.sh

---
+++ check_status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set up with a scheduler to regulary check 
+# Only if you want server status in discord (can be removed)
+discord_webhook_error="your webhook link"
+
+# Check if server is running
+if ! screen -list | grep -q "mc"; then
+        # Server is not running
+        echo "Server is not running"
+        curl -H "Content-Type: application/json" -d '{"content": "Error 103: Server is not running."}' $discord_webhook_error
+        exit 0
+fi       
